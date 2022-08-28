3a7900da35b976a27836c86f6c48d815116db4f3
Dojeto <omlimdiwala25@gmail.com>
2022-08-29 00:42:53 +0530

    test

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express")
-const ytdl = require("ytdl-core")
-const path = require("path")
+const express = require("express");
+const ytdl = require("ytdl-core");
+const path = require("path");
 const fs = require("fs");
-const deletefile = require("./utiles/deletefile")
-const app = express()
+const deletefile = require("./utiles/deletefile");
+const app = express();
 const port = process.env.PORT || 3000;
 
-var views = path.join(__dirname, "views");
+const views = path.join(__dirname, "views");
+
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
